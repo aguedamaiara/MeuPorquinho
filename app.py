8a968d9b6a4d661c1dc00741afcde48933bf35fc
Maiara Águeda <98491419+MaiaraAgueda@users.noreply.github.com>
2023-04-28 13:53:08 +0000

    video lofi

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ Ele exibe uma saudação simples e uma mensagem de boas-vindas.
 """
 import streamlit as st
 
-st.title("Meu primeiro aplicativo web ")
+st.title("Meu primeiro aplicativo web - Banco de Dados 2")
 st.title("Data: 28/04/2023")
 st.write("Lofi!")
 st.video("https://youtu.be/vC4dLeqnvAw")
