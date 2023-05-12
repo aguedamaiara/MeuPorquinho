ad1c0d9dadb4f58bd62e3c512332d23dae17dae2
cmscarla <105738672+cmscarla@users.noreply.github.com>
2023-05-12 13:09:20 +0000

    testando

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ import streamlit as st
 
 st.title("Meu primeiro aplicativo web - Banco de Dados 2")
 st.title("Data: 28/04/2023")
-st.write("Lofi!")
+st.write("TEST!")
 st.video("https://youtu.be/vC4dLeqnvAw")
